6f9dc4cff88248d9ace0f491993742b0d7296912
Niil Öhlin <niil.ohlin@izettle.com>
2019-01-12 18:44:17 +0100

    add operators to parser

--- Sources/Parser.swift
+++ Sources/Parser.swift
@@ -3,6 +3,54 @@ public struct Parser<T> {
     public let parse: (inout String) throws -> T
 }
 
+
+public extension Parser {
+    struct FailingParser: Error {
+    }
+    static func failingParser<T>(_ type: T.Type) -> Parser<T> {
+        return Parser<T> { _ in
+            throw FailingParser()
+        }
+    }
+}
+
+public extension Parser {
+    func map<G>(_ transform: @escaping (T) throws -> G) rethrows -> Parser<G> {
+        return Parser<G> { input in
+            let result = try self.parse(&input)
+            return try transform(result)
+        }
+    }
+
+//    func apply<G>(_ other: Parser<(T) -> G>) -> Parser<G> {
+//
+//    }
+
+    func flatMap<G>(_ transform: @escaping (T) throws -> Parser<G>) rethrows -> Parser<G> {
+        return Parser<G> { input in
+            var temp = input
+
+            let firstResult = try self.parse(&temp)
+            let newParser = try transform(firstResult)
+            let result = try newParser.parse(&temp)
+            input = temp
+            return result
+        }
+    }
+}
+
+public extension Parser {
+    static func or<T>(_ first: Parser<T>, second: Parser<T>) -> Parser<T> {
+        return Parser<T> { input in
+            do {
+                return try first.parse(&input)
+            } catch {
+                return try second.parse(&input)
+            }
+        }
+    }
+}
+
 public extension Parser where T == Character {
     struct WrongCharacterError: Error {
     }
@@ -15,8 +63,4 @@ public extension Parser where T == Character {
             return c
         }
     }
-}
-
-public extension Parser where T == String {
-    static func 
 }
--- Tests/RaskTests/ParserTests.swift
+++ Tests/RaskTests/ParserTests.swift
@@ -3,12 +3,60 @@ import Rask
 
 final class ParserTests: XCTestCase {
     func testParseChar() throws {
-        let charParser = Rask.Parser<Character>.character(Character("a"))
+        let charParser = Parser<Character>.character(Character("a"))
 
         var inputString = "apa"
 
         let parsed = try charParser.parse(&inputString)
         XCTAssertEqual(parsed, Character("a"))
         XCTAssertEqual(inputString, "pa")
+    }
+
+    func testMap() throws {
+        let charParser = Parser<Character>.character(Character("a"))
+        let singleStringParser = charParser.map(String.init)
+
+        var inputString = "apa"
+
+        let parsed = try singleStringParser.parse(&inputString)
+        XCTAssertEqual(parsed, "a")
+        XCTAssertEqual(inputString, "pa")
+    }
+
+    func testFlatMap() throws {
+        let charParser = Parser<Character>.character(Character("a"))
+        let abParser = charParser.flatMap { character in
+            Parser<Character>.character(character.nextInAlphabet())
+        }
+
+        var inputString = "aba"
+
+        let parsed = try abParser.parse(&inputString)
+        XCTAssertEqual(parsed, Character("b"))
+        XCTAssertEqual(inputString, "a")
+    }
+
+    func testFailingFlatMap() throws {
+        let charParser = Parser<Character>.character(Character("a"))
+        let abParser = charParser.flatMap { character in
+            Parser<Character>.character(character.nextInAlphabet())
+        }
+
+        var inputString = "apa"
+
+        _ = try? abParser.parse(&inputString)
+
+        XCTAssertEqual(inputString, "apa")
+
+    }
+}
+
+extension Character {
+    func nextInAlphabet() -> Character {
+        let alphabet = "abcdefghijklmnopqrstuvwxyz"
+        let indexOfSelf = alphabet.firstIndex(of: self)!
+        let indexOfNext = alphabet.index(indexOfSelf, offsetBy: 1)
+        return alphabet[indexOfNext]
+
     }
 }
